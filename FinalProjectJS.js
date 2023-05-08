3873a300fcf0318a97bf8c4b28821b2451f2d10c
Agenttb626 <97995029+Agenttb626@users.noreply.github.com>
2023-05-07 22:09:43 -0400

    Add files via upload

--- FinalProjectJS.js
+++ FinalProjectJS.js
@@ -5,3 +5,24 @@ fetch('FinalProjectFile.json')
   })
   .catch(error => console.error(error));
 
+  fetch('FinalProjectFile.json')
+  .then(response => response.json())
+  .then(data => {
+    const booksData = data.courses.flatMap(course => course.books);
+
+    const booksTable = document.getElementById('books-table');
+    booksData.forEach(book => {
+      const row = document.createElement('tr');
+      const titleCell = document.createElement('td');
+      titleCell.innerText = book.title;
+      const authorCell = document.createElement('td');
+      authorCell.innerText = book.author;
+      const publisherCell = document.createElement('td');
+      publisherCell.innerText = book.publisher;
+      row.appendChild(titleCell);
+      row.appendChild(authorCell);
+      row.appendChild(publisherCell);
+      booksTable.appendChild(row);
+    });
+  })
+  .catch(error => console.error(error));
