a845a74bec767845c9de278d9c9e82dffd8b3f9e
Agenttb626 <97995029+Agenttb626@users.noreply.github.com>
2023-05-07 21:34:38 -0400

    Add files via upload

--- FinalProjectJS.js
+++ FinalProjectJS.js
@@ -1,19 +1,7 @@
 fetch('FinalProjectFile.json')
   .then(response => response.json())
-  .then(course => {
-    const coursesList = document.getElementById('courses-list');
-    data.courses.forEach(course => {
-      const courseItem = document.createElement('li');
-      courseItem.textContent = course.title;
-      
-      const booksList = document.createElement('ul');
-      course.books.forEach(book => {
-        const bookItem = document.createElement('li');
-        bookItem.textContent = book.title;
-        booksList.appendChild(bookItem);
-      });
-      
-      courseItem.appendChild(booksList);
-      coursesList.appendChild(courseItem);
-    });
-  });
+  .then(data => {
+    const coursesData = data.courses;
+  })
+  .catch(error => console.error(error));
+
